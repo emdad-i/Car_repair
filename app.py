bebdf4e6c605b9fee1f946f833c11114328d3360
emdad-islam <38254873+emdad-i@users.noreply.github.com>
2025-08-02 22:38:21 +0100

    include moderation api checker

--- app.py
+++ app.py
@@ -11,20 +11,36 @@ st.write('Ask me how to repair your car!')
 user_input = st.text_input('Describe your car issue')
 
 if user_input:
-    headers = {
-        'Authorization': f'Bearer {openai_api_key}',
-        'Content-Type': 'application/json',
-    }
+        headers = {
+            'Authorization': f'Bearer {openai_api_key}',
+            'Content-Type': 'application/json',
+        }
 
-    data = {
-        "model": "gpt-4o",
-        "messages": [
-            {"role": "system", "content": "I am an AI trained to help with car repairs."},
-            {"role": "user", "content": user_input}
-        ]
-    }
+        data = {
+            'model': 'omni-moderation-latest',
+            'input': user_input
+        }
 
-    response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
-    response_json = response.json()
-    
-    st.write(response_json['choices'][0]['message']['content'])
+        mod_response = requests.post('https://api.openai.com/v1/moderations', headers=headers, data=json.dumps(data))
+
+        if bool(mod_response.json()['results'][0]['flagged']) == False:
+            headers = {
+                'Authorization': f'Bearer {openai_api_key}',
+                'Content-Type': 'application/json',
+            }
+
+            data = {
+                "model": "gpt-4o",
+                "messages": [
+                    {"role": "system", "content": "I am an AI trained to help with car repairs."},
+                    {"role": "user", "content": user_input}
+                ]
+            }
+
+            response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
+            response_json = response.json()
+            
+            st.write(response_json['choices'][0]['message']['content'])
+        else:
+              st.write('Moderation Failed')
+              st.write(mod_response.json())
